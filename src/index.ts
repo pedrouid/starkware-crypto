bd267300d48d75628ebd7d4b5f9d57d25a99c3b5
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-15 10:57:40 +0200

    getKeyPairFromPath

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
-/////////////////////////////////////////////////////////////////////////////////
-// Copyright 2019 StarkWare Industries Ltd.                                    //
-//                                                                             //
-// Licensed under the Apache License, Version 2.0 (the "License").             //
-// You may not use this file except in compliance with the License.            //
-// You may obtain a copy of the License at                                     //
-//                                                                             //
-// https://www.starkware.co/open-source-license/                               //
-//                                                                             //
-// Unless required by applicable law or agreed to in writing,                  //
-// software distributed under the License is distributed on an "AS IS" BASIS,  //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    //
-// See the License for the specific language governing permissions             //
-// and limitations under the License.                                          //
-/////////////////////////////////////////////////////////////////////////////////
-
 import BN from 'bn.js';
 import hash from 'hash.js';
+import assert from 'assert';
 import * as elliptic from 'elliptic';
 import { keccak_256 } from 'js-sha3';
-import assert from 'assert';
+import hdkey from 'ethereumjs-wallet/hdkey';
+
 import constantPointsHex from './constantPoints';
 
 /* --------------------------- TYPINGS ---------------------------------- */
@@ -188,6 +174,15 @@ function fixMessage(msg: string) {
 }
 
 /* --------------------------- PUBLIC ---------------------------------- */
+
+export function getKeyPairFromPath(seed: string, path: string): KeyPair {
+  const privateKey = hdkey
+    .fromMasterSeed(seed)
+    .derivePath(path)
+    .getWallet()
+    .getPrivateKey();
+  return getKeyPair(privateKey);
+}
 
 export function getKeyPair(privateKey: string): KeyPair {
   return starkEc.keyFromPrivate(privateKey, 'hex');
